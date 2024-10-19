66bbfc8f2dacb46a4a97ae83161bb0a1a06a4275
Gustavo-DCosta <gustagamerpereira@gmail.com>
2024-10-19 16:45:07 +0200

    Add files via upload

---
+++ find_files.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+import glob
+
+def path_exist():
+    path = r"C:\Users\veraf\Desktop\VsCode"
+    try:
+        if os.path.exists(path):
+            print(f"{path}: path does exist")
+            if os.path.isfile(path):
+                print("The path is a file")
+            elif os.path.isdir(path):
+                print("The path is a directory")
+        else:
+            print("The path does not exist")
+    except Exception as e:
+        print(f"Couldn't find anything. Error: {e}")
+
+path_exist()
+
+def match_exist():
+    try:    # Find all .py files in the current directory and subdirectories
+        py_files = glob.glob('**/*.py', recursive=True)
+        print(py_files)
+    except:
+        print("None found")
+ 
+match_exist()
+
+
+ 
